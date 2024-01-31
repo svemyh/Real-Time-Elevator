89e0ea4fecbe09cdfbd97ecad727afe5d7b8626c
Jonas <jonas@mail>
2024-01-31 16:31:45 +0100

    added elevio.Init, poll floor sensor, handle door timeout and timer in main.go

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"elevator/elevio"
+	"elevator/fsm"
+	"elevator/timer"
 	"fmt"
 	"time"
 )
@@ -20,7 +22,11 @@ func main() {
 	// for {
 	// 	// Poll request buttons
 	// 	for f := 0; f < elevio.N_Floors; f++ {
-	// 		for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
+	// 		for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {	// 	// Handle door timeout
+	// 	if timer.TimerTimedOut() {
+	// 		timer.TimerStop()
+	// 		fsm.FsmOnDoorTimeout()
+	// 	}
 	// 			if v := elevio.GetButton(b, f); v {
 	// 				fsm.FsmOnRequestButtonPress(f, b)
 	// 			}
@@ -50,15 +56,43 @@ func main() {
 	// elev.Config.DoorOpenDurationS = 3.0
 	var device elevio.ElevInputDevice
 
+	elevio.Init("localhost:15657", elevio.N_Floors)
+
+	fmt.Printf("Started!\n")
+
 	go elevio.PollFloorSensor(device.FloorSensorCh)
 	go elevio.PollButtons(device.RequestButtonCh)
 	go elevio.PollStopButton(device.StopButtonCh)
 	go elevio.PollObstructionSwitch(device.ObstructionCh)
 
-	// Run for a short period to demonstrate receiving signals
-	endTime := time.Now().Add(10 * time.Second)
+	// TODO(?): if input.floorSensor() == -1 {fsm_onInitBetweenFloors()} 
 
-	for time.Now().Before(endTime) {
+	// Run for a short period to demonstrate receiving signals. Check!
+	// endTime := time.Now().Add(10 * time.Second)
+	// for time.Now().Before(endTime) {
+	
+		for {
+		// Poll request buttons
+		 	for f := 0; f < elevio.N_Floors; f++ {
+		 		// for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
+				for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
+	 				if v := elevio.GetButton(b, f); v {
+	 					fsm.FsmOnRequestButtonPress(f, elevio.Button(b))
+		 				}
+	 			}
+	 		}
+
+		// Poll floor sensor
+	 		if f := elevio.GetFloor(); f != -1 {
+	 			fsm.FsmOnFloorArrival(f)
+		 	}
+
+		// Handle door timeout
+	 		if timer.TimerTimedOut() {
+	 			timer.TimerStop()
+	 			fsm.FsmOnDoorTimeout()
+			}
+
 		select {
 		case floor := <-device.FloorSensorCh:
 			fmt.Println("Floor Sensor:", floor)
