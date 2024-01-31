4c1adb06dd7d62678f8bb7c70ea5c3e906bfb1bf
Jonas <jonas@mail>
2024-01-31 19:07:00 +0100

    Asyncronous go-routines scanning for inputs fixed (PollFloorSensor and similar ones). Mistake found in wrong initialization of device of type ElevInputDevice.

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"elevator/elevio"
-	"elevator/fsm"
-	"elevator/timer"
 	"fmt"
 	"time"
 )
@@ -54,45 +52,52 @@ func main() {
 	// elev.Behaviour = elevator.EB_Idle                 /* ElevatorBehaviour value */
 	// elev.Config.ClearRequestVariant = elevator.CV_All /* ClearRequestVariant value */
 	// elev.Config.DoorOpenDurationS = 3.0
-	var device elevio.ElevInputDevice
+	//var device elevio.ElevInputDevice
 
 	elevio.Init("localhost:15657", elevio.N_Floors)
 
 	fmt.Printf("Started!\n")
+
+	device := elevio.ElevInputDevice{
+		FloorSensorCh:   make(chan int),
+		RequestButtonCh: make(chan elevio.ButtonEvent),
+		StopButtonCh:    make(chan bool),
+		ObstructionCh:   make(chan bool),
+	}
 
 	go elevio.PollFloorSensor(device.FloorSensorCh)
 	go elevio.PollButtons(device.RequestButtonCh)
 	go elevio.PollStopButton(device.StopButtonCh)
 	go elevio.PollObstructionSwitch(device.ObstructionCh)
 
-	// TODO(?): if input.floorSensor() == -1 {fsm_onInitBetweenFloors()} 
+	// TODO(?): if input.floorSensor() == -1 {fsm_onInitBetweenFloors()}
 
 	// Run for a short period to demonstrate receiving signals. Check!
 	// endTime := time.Now().Add(10 * time.Second)
 	// for time.Now().Before(endTime) {
-	
-		for {
-		// Poll request buttons
-		 	for f := 0; f < elevio.N_Floors; f++ {
-		 		// for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
-				for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
-	 				if v := elevio.GetButton(b, f); v {
-	 					fsm.FsmOnRequestButtonPress(f, elevio.Button(b))
-		 				}
-	 			}
-	 		}
 
-		// Poll floor sensor
-	 		if f := elevio.GetFloor(); f != -1 {
-	 			fsm.FsmOnFloorArrival(f)
-		 	}
+	// for {
+	// 	// Poll request buttons
+	// 	for f := 0; f < elevio.N_Floors; f++ {
+	// 		// for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
+	// 		for b := elevio.BT_HallUp; b <= elevio.BT_Cab; b++ {
+	// 			if v := elevio.GetButton(b, f); v {
+	// 				fsm.FsmOnRequestButtonPress(f, elevio.Button(b))
+	// 			}
+	// 		}
+	// 	}
 
-		// Handle door timeout
-	 		if timer.TimerTimedOut() {
-	 			timer.TimerStop()
-	 			fsm.FsmOnDoorTimeout()
-			}
+	// 	// Poll floor sensor
+	// 	if f := elevio.GetFloor(); f != -1 {
+	// 		fsm.FsmOnFloorArrival(f)
+	// 	}
 
+	// 	// Handle door timeout
+	// 	if timer.TimerTimedOut() {
+	// 		timer.TimerStop()
+	// 		fsm.FsmOnDoorTimeout()
+	// 	}
+	for {
 		select {
 		case floor := <-device.FloorSensorCh:
 			fmt.Println("Floor Sensor:", floor)
