091832ea9458eea834db85f895db686dcc02f3ce
Sorstronen <snorrehs@stud.ntnu.no>
2024-02-21 18:13:13 +0100

    refactor - cleaned up main

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"elevator/elevio"
 	"elevator/fsm"
-	"elevator/timer"
 	"fmt"
 	"time"
 )
 
 const inputPollRate = 25 * time.Millisecond
-
-var prev int = -1
 
 func main() {
 	elevio.Init("localhost:15657", elevio.N_Floors)
@@ -29,38 +26,7 @@ func main() {
 	go elevio.PollStopButton(device.StopButtonCh)
 	go elevio.PollObstructionSwitch(device.ObstructionCh)
 
-	if f := elevio.GetFloor(); f == -1 {
-		fsm.FsmOnInitBetweenFloors()
-	}
+	
+	fsm.FsmRun(device) 
 
-	// Polling for new actions/events of the system.
-	for {
-		select {
-		case floor := <-device.FloorSensorCh:
-			fmt.Println("Floor Sensor:", floor)
-			//var prev static int = 0
-			if floor != -1 && floor != prev {
-				fmt.Println("Floor Sensor1:")
-				fsm.FsmOnFloorArrival(floor)
-			}
-			fmt.Println("Floor Sensor2:")
-			prev = floor
-			break
-		case buttonEvent := <-device.RequestButtonCh:
-			fmt.Println("Button Pressed:", buttonEvent)
-			fsm.FsmOnRequestButtonPress(buttonEvent.Floor, elevio.Button(buttonEvent.Button))
-			break
-		case obstructionSignal := <-device.ObstructionCh:
-			fmt.Println("Obstruction Detected", obstructionSignal)
-			break
-
-		default:
-			// No action - prevents blocking on channel reads
-			time.Sleep(500 * time.Millisecond)
-		}
-		if timer.TimerTimedOut() { // should be reworked into a channel
-			timer.TimerStop()
-			fsm.FsmOnDoorTimeout()
-		}
-	}
 }
