63b6469f0a55b75f871830b99d9869df93398c0b
Sveinung Myhre <s-myhre@outlook.com>
2024-03-14 23:34:36 +0100

    fix: Type fix for previous commit: Cab requests no longer breaks the primary

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -54,7 +54,7 @@ func FsmOnRequestButtonPress(btnFloor int, btnType elevio.Button, FSMHallOrderCo
 		//println("Door Open")
 		if requests.ShouldClearImmediately(elevatorState, btnFloor, btnType) {
 			timer.TimerStart(5)
-			if btnType != elevio.BT_Cab { // FSMHallOrderCompleteCh only accepts
+			if btnType != elevio.B_Cab { // FSMHallOrderCompleteCh only accepts
 				FSMHallOrderCompleteCh <- elevio.ButtonEvent{Floor: btnFloor, Button: elevio.ButtonType(btnType)}
 			}
 		} else {
