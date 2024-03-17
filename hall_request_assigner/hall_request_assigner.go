75055b81ca4787c4b8b9210be9423688dec4abcb
0 <0>
2024-03-17 18:09:32 +0100

    test: testing previous implementations

--- hall_request_assigner/hall_request_assigner.go
+++ hall_request_assigner/hall_request_assigner.go
@@ -5,6 +5,7 @@ import (
 	"elevator/elevio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"os/exec"
 	"runtime"
@@ -121,7 +122,17 @@ func HallRequestAssigner(ActiveElevatorsMap map[string]elevator.Elevator, Combin
 		panic("OS not supported")
 	}
 
-	input := ActiveElevators_to_HRAInput(ActiveElevatorsMap, CombinedHallRequests)
+	filteredActiveElevatorsMap := make(map[string]elevator.Elevator)
+	log.Println("HALL REQUEST ASSIGNER ActiveElevatorsMap:", ActiveElevatorsMap)
+	for ip, elev := range ActiveElevatorsMap {
+		if elev.Available { // Check if the elevator is marked as available
+			filteredActiveElevatorsMap[ip] = elev
+		}
+	}
+	log.Println("HALL REQUEST ASSIGNER filteredActiveElevatorsMap:", filteredActiveElevatorsMap)
+
+
+	input := ActiveElevators_to_HRAInput(filteredActiveElevatorsMap, CombinedHallRequests)
 
 	jsonBytes, err := json.Marshal(input)
 	if err != nil {
