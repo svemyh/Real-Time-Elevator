0c411e96f3d4b58adefa4032e36a10710cfa30f0
0 <0>
2024-03-17 18:38:38 +0100

    test: added logs and filtered some activeElevatorMaps

--- network/primary.go
+++ network/primary.go
@@ -228,6 +228,7 @@ func HandlePrimaryTasks(ActiveElevatorMap map[string]elevator.Elevator,
 				filteredActiveElevatorMap[ip] = elev
 			}
 		}
+		log.Println("HALL REQUEST ASSIGNER filteredActiveElevatorsMap:", filteredActiveElevatorMap)
 
 		fmt.Println("~~ HandlePrimaryTasks() - ActiveElevatorMap: ", filteredActiveElevatorMap)
 		fmt.Println("~~ HandlePrimaryTasks() - CombinedHallRequests: ", CombinedHallRequests)
@@ -235,7 +236,7 @@ func HandlePrimaryTasks(ActiveElevatorMap map[string]elevator.Elevator,
 		case stateUpdate := <-StateUpdateCh:
 			fmt.Println("StateUpdate: ", stateUpdate)
 			filteredActiveElevatorMap[stateUpdate.MyAddress] = stateUpdate.Elevator
-			if len(ActiveElevatorMap) >= 2 {
+			if len(filteredActiveElevatorMap) >= 2 {
 				if _, exists := filteredActiveElevatorMap[BackupAddr]; !exists {
 					fmt.Println("Backup does not exists yet. Initializing it..")
 					BackupAddr = GetBackupAddress(filteredActiveElevatorMap)
@@ -264,10 +265,10 @@ func HandlePrimaryTasks(ActiveElevatorMap map[string]elevator.Elevator,
 			CombinedHallRequests[completedOrder.Floor][completedOrder.Button] = false
 			BroadcastCombinedHallRequestsCh <- CombinedHallRequests
 
-			if len(ActiveElevatorMap) >= 2 {
-				if _, exists := ActiveElevatorMap[BackupAddr]; !exists {
+			if len(filteredActiveElevatorMap) >= 2 {
+				if _, exists := filteredActiveElevatorMap[BackupAddr]; !exists {
 					fmt.Println("Backup does not exists yet. Initializing it..")
-					BackupAddr = GetBackupAddress(ActiveElevatorMap)
+					BackupAddr = GetBackupAddress(filteredActiveElevatorMap)
 					backupConn = TCPDialBackup(BackupAddr, TCP_BACKUP_PORT)
 					go TCPReadACK(backupConn, DisconnectedElevatorCh, AckCh) // Using the established backupConn start listening for ACK's from Backup.
 				}
